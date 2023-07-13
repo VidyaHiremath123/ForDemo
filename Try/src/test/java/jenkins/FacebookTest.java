8f5f69a4d567df6eb61931ea517db82b490a713f
VidyaHiremath123 <gowri.spn@gmail.com>
2023-07-13 17:08:05 +0530

    213

--- Try/src/test/java/jenkins/FacebookTest.java
+++ Try/src/test/java/jenkins/FacebookTest.java
@@ -2,6 +2,7 @@ package jenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import genUtility.BaseClass;
@@ -10,8 +11,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class FacebookTest  {
 	@Test
 	public void facebook() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		//WebDriverManager.chromedriver().setup();
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origin=*");
+		WebDriver driver=new ChromeDriver(co);
 		driver.get("https://www.amazon.in/");
 		System.out.println("hello");
 		
