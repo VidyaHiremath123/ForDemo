5eb17f816fd2e36d3470a8a6be494fa81113f9f4
VidyaHiremath123 <gowri.spn@gmail.com>
2023-07-13 13:40:33 +0530

    4

---
+++ Try/src/test/java/jenkins/Demo2Test.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("GoodMorning");
+		
+	}
+}
---
+++ Try/src/test/java/jenkins/Demo3Test.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class Demo3Test {
+	@Test
+	public void demo3test()
+	{
+		System.out.println("GoodEvening");
+	}
+
+}
